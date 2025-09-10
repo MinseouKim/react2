20cce03defed35a551d1741202d1a96954aff0a4
MinseouKim <202130103@email.daelim.ac.kr>
2025-09-10 12:10:29 +0900

    9/10_2교시까지내용업로드

---
+++ my-app/src/app/(marketing)/(shop)/page.tsx
@@ -0,0 +1,8 @@
+export default function Shop(){
+    return(
+        <div>
+            <h1 className="text-2x1 font-bold" >Dashboard</h1>
+            <p>Welcome to the dashboard page!</p>
+        </div>
+    )
+}
---
+++ my-app/src/app/(marketing)/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About(){
+    return(
+        <div>
+            <h1 className="text-2x1 font-bold" >Dashboard</h1>
+            <p>Welcome to the dashboard page!</p>
+        </div>
+    )
+}
